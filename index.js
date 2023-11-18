bf41021e6dcab2b34d44336797b2222f51383224
Mateusz Jarosz <mati5033onet@gmail.com>
2023-11-18 21:01:14 +0100

    button

--- index.js
+++ index.js
@@ -1,6 +1,6 @@
 //declarations
 let siteName = 'Project';
-let author = 'Mateusz';
+let author = 'Mateusz Jarosz';
 let daysPassed = 1;
 let finished = false;
 
@@ -8,5 +8,36 @@ let finished = false;
 
 document.getElementById('id_1').innerHTML = `Welcome to ${siteName}`;
 document.getElementById('id_2').innerHTML = `The author of ${siteName} is ${author}`;
-document.getElementById('id_3').innerHTML = `Started ${daysPassed} ago`;
 document.title = siteName;
+
+//let's start button
+
+document.addEventListener('DOMContentLoaded', function() {
+  let modal = document.getElementById('myModal');
+  let btn = document.getElementById('id_3');
+  let span = document.getElementsByClassName('close')[0];
+
+  btn.onclick = function() {
+    modal.style.display = 'block';
+  }
+
+  span.onclick = function() {
+    modal.style.display = 'none';
+  }
+
+  window.onclick = function(event) {
+    if (event.target == modal) {
+      modal.style.display = 'none';
+    }
+  }
+});
+
+function processText() {
+  var inputText = document.getElementById('textInput').value;
+  // Tutaj dodaj kod JavaScript do przetwarzania tekstu
+  // np. wywołaj odpowiednie funkcje, analizuj tekst, itp.
+  var resultDiv = document.getElementById('result');
+  // Poniżej jest tylko przykład, wstaw do tej zmiennej rzeczywiste wyniki
+  var result = 'Wyniki przetwarzania: ' + inputText.toUpperCase();
+  resultDiv.innerHTML = result;
+}
